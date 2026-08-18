1175d9cb7b47414da1c9d7504bab71bc525e5de1
Keii-Del <gudgame.314@gmail.com>
2026-08-18 19:42:33 +0700

    fixing some bug

--- src/components/CharacterDisplay.jsx
+++ src/components/CharacterDisplay.jsx
@@ -1,18 +1,60 @@
 import { getCharClass } from "../utils/typingHelpers";
 
 function CharacterDisplay({ characters, typedText }) {
+  const words = [];
+  let currentWord = [];
+
+  characters.forEach((char) => {
+    if (char === " ") {
+      words.push(currentWord);
+      currentWord = [];
+    } else {
+      currentWord.push(char);
+    }
+  });
+  words.push(currentWord);
+
+  let globalIndex = 0;
+
   return (
     <div className="max-w-3xl flex flex-wrap">
-      {characters.map((x, z) => {
-        const isCurrent = z === typedText.length;
+      {words.map((word, wordIndex) => {
+        const isLastWord = wordIndex === words.length - 1;
+
         return (
-          <span
-            key={z}
-            className={`${getCharClass(x, z, typedText)} whitespace-pre ${
-              isCurrent ? "border-l-2 border-yellow-400 animate-pulse" : ""
-            }`}
-          >
-            {x}
+          <span key={wordIndex} className="inline-flex">
+            {word.map((x) => {
+              const z = globalIndex;
+              globalIndex++;
+              const isCurrent = z === typedText.length;
+              return (
+                <span
+                  key={z}
+                  className={`${getCharClass(x, z, typedText)} whitespace-pre ${
+                    isCurrent ? "border-l-2 border-yellow-400 animate-pulse" : ""
+                  }`}
+                >
+                  {x}
+                </span>
+              );
+            })}
+
+            {!isLastWord &&
+              (() => {
+                const z = globalIndex;
+                globalIndex++;
+                const isCurrent = z === typedText.length;
+                return (
+                  <span
+                    key={`space-${wordIndex}`}
+                    className={`${getCharClass(" ", z, typedText)} whitespace-pre ${
+                      isCurrent ? "border-l-2 border-yellow-400 animate-pulse" : ""
+                    }`}
+                  >
+                    {" "}
+                  </span>
+                );
+              })()}
           </span>
         );
       })}
